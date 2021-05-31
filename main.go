af6e20621fcacb594e83afe60182e4504ee1b018
David Sauer <david.sauer@turbinekreuzberg.com>
2021-05-31 19:30:48 +0200

    wrapped functionality into cli tool

--- main.go
+++ main.go
@@ -1,38 +1,89 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 
+	"github.com/spf13/afero"
+	"github.com/urfave/cli/v2"
 	prettier "github.com/utopia-planitia/chart-prettier/pkg"
 )
 
 func main() {
-	err := run()
+	err := run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
-	yml, err := ioutil.ReadAll(os.Stdin)
+func run(args []string) error {
+	app := &cli.App{
+		Name:   "chart-prettier",
+		Usage:  "sort files in chart directories",
+		Action: cleanupChart,
+	}
+
+	err := app.Run(args)
 	if err != nil {
 		return err
 	}
 
-	manifests, err := prettier.SplitManifests(string(yml))
+	return nil
+}
+
+func cleanupChart(c *cli.Context) error {
+	stdin, err := detectStdinPipe()
 	if err != nil {
-		return err
+		return fmt.Errorf("detect pipe to stdin: %v", err)
 	}
 
-	for _, manifest := range manifests {
-		_, err = fmt.Printf("---\n%s\n", manifest.Yaml)
+	if stdin && c.Args().Len() != 1 {
+		return fmt.Errorf("using stdin only supports exactly one chart directory")
+	}
+
+	appFs := afero.NewOsFs()
+
+	for _, path := range c.Args().Slice() {
+		chart := &prettier.Chart{}
+
+		err := chart.LoadChart(appFs, path)
 		if err != nil {
-			return err
+			return fmt.Errorf("loading manifests from existing chart: %v", err)
+		}
+
+		if stdin {
+			buf := bytes.Buffer{}
+			buf.ReadFrom(os.Stdin)
+
+			err = chart.AddManifests(string(buf.Bytes()))
+			if err != nil {
+				return fmt.Errorf("loading manifests from stdin: %v", err)
+			}
+		}
+
+		err = chart.DeleteFiles(appFs, path)
+		if err != nil {
+			return fmt.Errorf("cleanup preexisting manifests: %v", err)
+		}
+
+		err = chart.WriteOut(appFs, path)
+		if err != nil {
+			return fmt.Errorf("create new manifests in chart: %v", err)
 		}
 	}
 
 	return nil
+}
+
+func detectStdinPipe() (bool, error) {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return false, err
+	}
+
+	pipedToStdin := (stat.Mode() & os.ModeCharDevice) == 0
+
+	return pipedToStdin, nil
 }
